030a930e05d8cc3e67630a2fdb9ff9b9f4dd440d
Suraj <surajtadhani@gmail.com>
2024-08-08 12:43:18 +0530

    Changing in setting.py file

--- InfinityFlora/settings.py
+++ InfinityFlora/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = True  
 
 ALLOWED_HOSTS = ['.vercel.app']
 
@@ -140,7 +140,7 @@ RAZORPAY_SECRET = os.environ.get('RAZORPAY_SECRET')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+EMAIL_USE_TLS = True
