87848cda6f2a581af2a8952a80954b713635daf8
Suraj <surajtadhani@gmail.com>
2024-08-08 11:08:27 +0530

    Modified some files

--- InfinityFlora/settings.py
+++ InfinityFlora/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
